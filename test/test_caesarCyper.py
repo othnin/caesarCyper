d721f1e0b288cd99875ef0c25728379bc3b6acde
chris <cgrahlmann@signaturescience.com>
2021-09-23 15:22:15 -0500

    Removed fail test

--- test/test_caesarCyper.py
+++ test/test_caesarCyper.py
@@ -5,8 +5,8 @@ class TestCyper(unittest.TestCase):
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
 
-    def test_upper_fail(self):
-        self.assertEqual('foo'.upper(), 'BAD')
+ #   def test_upper_fail(self):
+ #       self.assertEqual('foo'.upper(), 'BAD')
 
 
 if __name__ == '__main__':
