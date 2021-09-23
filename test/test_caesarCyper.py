42102f28368d6163f81848a37e9854af78d307d4
chris <cgrahlmann@signaturescience.com>
2021-09-23 15:12:21 -0500

    Added fail test case

--- test/test_caesarCyper.py
+++ test/test_caesarCyper.py
@@ -5,6 +5,9 @@ class TestCyper(unittest.TestCase):
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
 
+    def test_upper_fail(self):
+        self.assertEqual('foo'.upper(), 'BAD')
+
 
 if __name__ == '__main__':
     unittest.main()
